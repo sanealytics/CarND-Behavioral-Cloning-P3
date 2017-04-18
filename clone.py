09a955b506a3c782d3718538df91c697f5cc0563
Saurabh RTR <saurabh@renttherunway.com>
2017-04-18 02:04:20 -0400

    NVIDIA model

--- clone.py
+++ clone.py
@@ -34,25 +34,33 @@ y_train = np.array(measurements)
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import Conv2D, MaxPooling2D, Cropping2D
-#from keras.backend import tf
+import matplotlib.pyplot as plt
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160, 320, 3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
-#model.add(Convolution2D(6, 5, 5, subsample = (2,2), activation = 'relu'))
-model.add(Conv2D(6, (5, 5), strides=(2, 2), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Convolution2D(6, 5, 5, subsample = (2,2), activation = 'relu'))
-model.add(Conv2D(6, (5, 5), strides=(2, 2), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Conv2D(48, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu"))
 model.add(Flatten())
-model.add(Dense(128, activation = 'relu'))
-model.add(Dense(84, activation = 'relu'))
+model.add(Dense(100, activation = 'relu'))
+model.add(Dense(50, activation = 'relu'))
+model.add(Dense(10, activation = 'relu'))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
+history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
 
 print('saving model')
 model.save('model.h5')
+
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plot.ylabel('mean squared error loss')
+plot.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.save('loss.png')
 
