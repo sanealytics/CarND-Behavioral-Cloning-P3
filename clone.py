cadab15fb3fd2f59f446bd8d2acbe806221acf4b
Saurabh RTR <saurabh@renttherunway.com>
2017-04-18 21:42:40 -0400

    Add augmented data and save on every epoch

--- clone.py
+++ clone.py
@@ -8,8 +8,6 @@ import random
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     correction = 0.2
-    image_x = 320
-    image_y = 160
     while True: # Forever
         shuffle(samples)
         for offset in range(0, num_samples, batch_size):
@@ -21,20 +19,22 @@ def generator(samples, batch_size=32):
                 for i in range(3):
                     source_path = batch_sample[i]
                     filename = source_path.split('/')[-1]
-                    #if (len(source_path.split('/') > 2)): # TODO: Change hack to better parse
-                    #    image_path = 'data/run1/IMG/'
-                    #else:
-                    #    image_path = 'data/data/IMG/'
-                    image_path = 'data/data/IMG/'
+                    if len(source_path.split('/')) > 2: 
+                        image_path = 'data/' + source_path.split('/')[-3] + '/IMG/'
+                    else:
+                        image_path = 'data/provided/IMG/'
                     current_path = image_path + filename
                     image = cv2.imread(current_path)
                     # Crop
+                    # image_x = 320
+                    # image_y = 160
                     # images.append(image[60:(image_y - 20)]) # Don't use - Crop in model instead
                     measurement = float(batch_sample[3])
                     if i == 1: # left
                         measurement += correction # Steer right
                     if i == 2: # Right
                         measurement -= correction # Steer left
+                    # Simulate driving the other way
                     if random.random() < 0.5: # Randomly flip
                         image = cv2.flip(image, 1) # Around y-axis
                         measurement = -measurement
@@ -54,25 +54,26 @@ from keras.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
 
 print('Reading data')
+input_csvs = ['data/provided/driving_log.csv', \
+        'data/lap_reverse/driving_log.csv', \
+        'data/curves/driving_log.csv']
 lines = []
-with open('data/data/driving_log.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile)
-    next(reader) # Skip header
-    for line in reader:
-        lines.append(line)
+for filename in input_csvs:
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader) # Skip header
+        for line in reader:
+            lines.append(line)
 
 
 train_samples, validation_samples = train_test_split(lines, test_size = 0.2)
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
-ch, row, col = 3, 80, 320 # Trimmed image format
 
 model = Sequential()
-#model.add(Lambda(lambda x: x / 127.5 - 1., input_shape = (ch, row, col), output_shape = (ch, row, col)))
-#model.add(Lambda(lambda x: x / 127.5 - 1., input_shape = (160, 320, 3)))
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160, 320, 3)))
-model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Cropping2D(cropping=((70,25), (0,0)))) # Doing this here so same thing happens in predict mode
 
 model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu"))
 model.add(BatchNormalization())# Added Batchnorm
@@ -87,7 +88,7 @@ model.add(BatchNormalization())# Added Batchnorm
 
 model.add(Conv2D(64, (1, 1), activation="relu")) # Added 1x1 convolutions
 
-model.add(Conv1D(64, (3, 3), activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu"))
 model.add(BatchNormalization())# Added Batchnorm
 
 model.add(Conv2D(64, (3, 3), activation="relu"))
@@ -106,14 +107,13 @@ model.add(Dense(1))
 
 print('Training model')
 model.compile(loss='mse', optimizer='adam')
-checkpointer = ModelCheckpoint(filepath="model_checkpoint.hdf5", verbose=1, save_best_only=True)
-
+checkpointer = ModelCheckpoint(filepath="model_checkpoint_{epoch:02d}_{val_loss:.2f}.hdf5", verbose=1)
 
 history_object = model.fit_generator(train_generator, steps_per_epoch=len(train_samples), \
         validation_data = validation_generator, validation_steps = len(validation_samples), epochs=10, callbacks=[checkpointer]) 
 
 print('saving model')
-model.save('model.h5')
+model.save('final_model.h5')
 
 print('Plotting curves')
 
@@ -124,5 +124,5 @@ plt.title('model mean squared error loss')
 plot.ylabel('mean squared error loss')
 plot.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
-plt.save('loss.png')
+plt.savefig('loss.png')
 
